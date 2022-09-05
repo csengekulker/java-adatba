943f85ecd229a2bfbe1d6aba1ec7974d9d54fa3c
csengekulker <k71608883429@ktch.hu>
2022-09-05 12:07:54 +0200

    todo: handle SQLexcpetion

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+
+        //independent of db type 
+        Data data = new Data(new MariaDB());
+
+        data.connect();
+
+        System.out.println("Connection established(?)");
     }
 }
---
+++ src/Data.java
@@ -0,0 +1,13 @@
+public class Data {
+
+    //later: SQLite 
+    Database db;
+
+    public Data(Database db) {
+        this.db = db;
+    }
+
+    public void connect() {
+        this.db.connect();
+    }
+}
