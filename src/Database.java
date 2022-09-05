40a340607bebf3840b550e9ffe68d8fc4e226afc
csengekulker <k71608883429@ktch.hu>
2022-09-05 11:59:15 +0200

    db connection, exception handled

--- src/Database.java
+++ src/Database.java
@@ -1,10 +1,11 @@
 
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public interface Database {
 
-    public Connection connect();
-    public void close(Connection conn);
+    public Connection connect() throws SQLException;
+    public void close(Connection conn) throws SQLException;
 
 }
--- src/MariaDB.java
+++ src/MariaDB.java
@@ -1,11 +1,33 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MariaDB implements Database{
-    public Connection connect() {
+    public Connection tryConnect() throws SQLException {
 
+            Connection conn = null;
+            String url = "jdb:mariadb://localhost:3306/adatba";
+            String usr = "adatba";
+            String psw = "titok";
+    
+            conn = DriverManager.getConnection(url, usr, psw);
+
+            return conn;
     }
 
-    public void close() {
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = tryConnect();
+
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+
+        return conn;
+    }
+
+    public void close(Connection conn) {
 
     }
 }
