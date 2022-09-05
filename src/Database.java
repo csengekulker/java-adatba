303520c45666f308a5402ea841b73f4e16e04d39
csengekulker <k71608883429@ktch.hu>
2022-09-05 11:36:36 +0200

    Database implemented

--- src/Database.java
+++ src/Database.java
@@ -1,10 +1,10 @@
-package api;
+
 
 import java.sql.Connection;
 
 public interface Database {
 
     public Connection connect();
-    public void close();
-    
+    public void close(Connection conn);
+
 }
---
+++ src/MariaDB.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+
+public class MariaDB implements Database{
+    public Connection connect() {
+
+    }
+
+    public void close() {
+
+    }
+}
