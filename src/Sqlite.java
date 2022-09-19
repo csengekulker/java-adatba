c82e6d7be5be68e94cecf1262b982a7f2fcb766f
csengekulker <k71608883429@ktch.hu>
2022-09-19 09:15:49 +0200

    sqlite open/close db

--- src/Sqlite.java
+++ src/Sqlite.java
@@ -1,18 +1,42 @@
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.DriverManager;
 
 public class Sqlite implements Database{
 
+  public Connection tryConnectDB() throws SQLException {
+    String url = "jdbc:sqlite:adatba.sqlite";
+
+    return DriverManager.getConnection(url);
+  }
+
   @Override
-  public Connection connectDB() throws SQLException {
-    // TODO Auto-generated method stub
-    return null;
+  public Connection connectDB() {
+
+    Connection conn = null;
+
+    try {
+      conn = tryConnectDB();
+    } catch (SQLException e) {
+      System.err.println(e);
+    }
+
+    return conn;
+  }
+
+  public void tryCloseDB(Connection conn ) throws SQLException {
+    conn.close();
   }
 
   @Override
   public void closeDB(Connection conn) throws SQLException {
-    // TODO Auto-generated method stub
-    
+    try {
+      tryCloseDB(conn );
+    } catch (Exception e) {
+      // TODO: handle exception
+      System.err.println(e);
+    }
   }
+
 
 }
