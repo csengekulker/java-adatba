6e1ba6ba8be080334d0cd7e016d36def6d731e43
csengekulker <k71608883429@ktch.hu>
2022-09-19 09:02:39 +0200

    sqlite init

---
+++ db/sqlite/create_table.sql
@@ -0,0 +1,7 @@
+-- SQLite
+CREATE TABLE employees (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  city TEXT,
+  salary REAL
+);
---
+++ src/Sqlite.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Sqlite implements Database{
+
+  @Override
+  public Connection connectDB() throws SQLException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void closeDB(Connection conn) throws SQLException {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
